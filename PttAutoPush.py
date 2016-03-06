7bd54d682edcc6110845791a0d9f203db82a2d3a
twtrubiks <twtrubiks@gmail.com>
2016-03-06 20:07:37 +0800

    create

---
+++ PttAutoPush.py
@@ -0,0 +1,132 @@
+#coding=utf-8
+import telnetlib
+import sys
+import time
+
+host = 'ptt.cc'
+user = 'Your PTT ID'
+password = 'Your PTT Password'
+
+
+def login(host, user ,password) :
+    global telnet
+    telnet = telnetlib.Telnet(host)
+    time.sleep(1)
+    content = telnet.read_very_eager().decode('big5','ignore')
+    if u"系統過載" in content :
+        print "系統過載, 請稍後再來"
+        sys.exit(0)
+        
+
+    if u"請輸入代號" in content:
+        print "輸入帳號中..."
+        telnet.write(user + "\r\n" )
+        time.sleep(1)
+        print "輸入密碼中..."
+        telnet.write(password + "\r\n")
+        time.sleep(5)
+        content = telnet.read_very_eager().decode('big5','ignore')
+        #print content
+        if u"密碼不對" in content:
+           print "密碼不對或無此帳號。程式結束"
+           sys.exit()
+           content = telnet.read_very_eager().decode('big5','ignore')
+        if u"您想刪除其他重複登入" in content:
+           print '刪除其他重複登入的連線....'
+           telnet.write("y\r\n")
+           time.sleep(10)
+           content = telnet.read_very_eager().decode('big5','ignore')
+        if u"請按任意鍵繼續" in content:
+           print "資訊頁面，按任意鍵繼續..."
+           telnet.write("\r\n" )
+           time.sleep(2)
+           content = telnet.read_very_eager().decode('big5','ignore')
+        if u"您要刪除以上錯誤嘗試" in content:
+           print "刪除以上錯誤嘗試..."
+           telnet.write("y\r\n")
+           time.sleep(5)
+           content = telnet.read_very_eager().decode('big5','ignore')
+        if u"您有一篇文章尚未完成" in content:
+           print '刪除尚未完成的文章....'
+           # 放棄尚未編輯完的文章
+           telnet.write("q\r\n")   
+           time.sleep(5)   
+           content = telnet.read_very_eager().decode('big5','ignore')
+        print "----------------------------------------------"
+        print "------------------ 登入完成 ------------------"
+        print "----------------------------------------------"
+        
+    else:
+        print "沒有可輸入帳號的欄位，網站可能掛了"
+
+def disconnect() :
+     print "登出中..."
+     # q = 上一頁，直到回到首頁為止，g = 離開，再見
+     telnet.write("qqqqqqqqqg\r\ny\r\n" )
+     time.sleep(3)
+     #content = telnet.read_very_eager().decode('big5','ignore')
+     #print content
+     print "----------------------------------------------"
+     print "------------------ 登出完成 ------------------"
+     print "----------------------------------------------"
+     telnet.close()
+
+def post(board, push, index, tag) :
+        list_AID = []
+        # s 進入要發文的看板
+        telnet.write('s')
+        telnet.write((board + '\r\n').encode('big5'))
+        time.sleep(1)       
+        telnet.write("q")                            
+        time.sleep(1)        
+        reset_count = 0;      
+        while 1 : 
+           print '機器人推文中...'
+           if(reset_count == index):
+              reset_count = 0 ;
+              telnet.write('$') ; # "$" = End = 到最末頁
+
+           telnet.write('Q') #查詢文章資訊
+           time.sleep(2)
+           content = telnet.read_very_eager().decode('big5','ignore')
+           if( (u'特殊文章，無價格記錄' not in content) and (u'文章代碼(AID)' in content) ):          
+              start = content.find(u'文章代碼(AID)')
+              end = content.find(u'這一篇文章')
+              contentdata = content[start:end]        
+              start = contentdata.find('#')
+              end = contentdata.find( '('+board )
+              AID = contentdata[start:end].strip();
+              if (AID not in list_AID ):
+                 list_AID.append(AID)
+                 telnet.write('\r\nX')   # "X" = 推文
+                 time.sleep(2)              
+                 content = telnet.read_very_eager().decode('big5','ignore')   
+                 if (u'您覺得這篇文章' in content):
+                    telnet.write( (tag + push +'\r\ny\r\n').encode('big5') ) #tag = 1(推) or(噓) 2 or 3(註解)
+                 else:
+                    telnet.write( (push+'\r\ny\r\n').encode('big5') )     
+           else: 
+               telnet.write('\r\n') 
+
+           time.sleep(3)  
+           telnet.write('p') # 'p' = 方向鍵的上
+           reset_count += 1;
+        print "----------------------------------------------"
+        print '自動推文完成...'
+        print "----------------------------------------------"
+ 
+
+def main():
+    login(host, user ,password)    
+
+    #python PttAutoPush.py gossiping  board  index tag
+    #python PttAutoPush.py gossiping '別再發文了，趕快出門運動吧'  10 1
+    board, push, index, tag = sys.argv[1],sys.argv[2].decode('utf-8'),int(sys.argv[3]),sys.argv[4]
+    post(board, push, index, tag)
+    #disconnect()     
+       
+
+if __name__=="__main__" :
+   main()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # PttAutoPush
-PTT自動推文 推文機器人(Python) 
+PTT自動推文 推文機器人(Python)
+
+## 特色
+* 推文機器人
+* 持續至特定看板對最新文章推文
+
+   
+## 使用方法
+
+* [Demo Video](https://www.youtube.com/watch?v=vyLUPSfCprc) - Linux 
+
+請將下列程式碼修改為自己的PTT ID 以及 Password
+
+```
+user = 'Your PTT ID'
+password = 'Your PTT Password'
+```
+
+```
+$ python PttAutoPush.py [版名] [內容] [標題數] [推文tag]
+
+```
+說明:
+
+[版名] : 不分大小寫，EX. gossiping
+
+[內容] : 推文內容，內容須加上 " "，EX. "推文測試"
+
+[標題數] : 對最新的[標題數]筆文章推文，不包含至底文
+
+[推文tag] : 1 = 推文 or 2 = 噓文 or 3 = 註解
+
+
+
+###執行環境
+Ubuntu 12.04
+
+Python 2.7.3
+
+
+
+## 執行範例 
+
+``` 
+$ python PttAutoPush.py gossiping '別再發文了，趕快出門運動吧'  10 1
+```
+
+該範例為在PTT gossiping(八卦)板  自動對最新的10篇文章推文
+
+推文的內容為 '別再發文了，趕快出門運動吧'
+
+程式永遠不會停止
+
+除非使用 任何軟體登入PTT，使程式執行錯誤 或 輸入Ctrl + C '強制終止程式
+
+
+執行過程
+
+![alt tag](http://i.imgur.com/sRMIyag.jpg)
+
+輸出 (使用pcman觀看)
+
+![alt tag](http://i.imgur.com/hgKup56.jpg)
+
+# License
+MIT license
+
